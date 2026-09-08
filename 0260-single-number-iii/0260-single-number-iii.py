984de3b4cc368a411629881d8f423f1814db5cb7
Akhand Pratap singh <148524224+Akhandbhadouria@users.noreply.github.com>
2026-09-08 10:38:50 +0530

    Time: 3175 ms (5.36%), Space: 13 MB (84.43%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans=[]
+        for i in nums:
+            if(nums.count(i)==1):
+                ans.append(i)
+        return ans
+        
